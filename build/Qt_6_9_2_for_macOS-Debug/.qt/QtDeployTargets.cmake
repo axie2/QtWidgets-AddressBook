196664bd68a0fde4c94dcd6df37afc319de43ae9
Arianna Xie <ariannaxie7@gmail.com>
2025-09-18 16:05:25 -0400

    Removed build folder from repository

--- build/Qt_6_9_2_for_macOS-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_addressbook_FILE /Applications/Qt/Examples/Qt-6.9.2/widgets/itemviews/addressbook/build/Qt_6_9_2_for_macOS-Debug/addressbook.app/Contents/MacOS/addressbook)
-set(__QT_DEPLOY_TARGET_addressbook_TYPE EXECUTABLE)
--- build/Qt_6_9_2_for_macOS-Debug/.qt/deploy_addressbook_178b218333.cmake
+++
@@ -1,7 +0,0 @@
-include("/Applications/Qt/Examples/Qt-6.9.2/widgets/itemviews/addressbook/build/Qt_6_9_2_for_macOS-Debug/.qt/QtDeploySupport.cmake")
-include("${CMAKE_CURRENT_LIST_DIR}/addressbook-plugins.cmake" OPTIONAL)
-set(__QT_DEPLOY_I18N_CATALOGS "qtbase")
-
-qt6_deploy_runtime_dependencies(
-    EXECUTABLE "addressbook.app"
-)
